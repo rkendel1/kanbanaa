114da403a366ccc4cf9c9de70094d455a0c2137e
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-15 13:58:40 -0400

    Adopt repository: src/context/KanbanContextTypes.ts

---
+++ src/context/KanbanContextTypes.ts
@@ -0,0 +1,43 @@
+import { DropResult } from "react-beautiful-dnd";
+import { KanbanBoardState, KanbanCard } from "../components/kanban/KanbanTypes";
+import { CardModalProps } from "../components/modal/CardModal";
+import { DeleteListModalProps } from "../components/modal/DeleteListModal";
+import { RenameListModalProps } from "../components/modal/RenameListModal";
+
+export type ModalTypes = "DELETE_LIST" | "UPDATE_CARD" | "RENAME_LIST";
+
+export type ModalContextState = {
+  type: ModalTypes | null;
+  isOpen: boolean;
+  modalProps:
+    | DeleteListModalProps
+    | CardModalProps
+    | RenameListModalProps
+    | null;
+};
+
+export type hanbleOpenModalProps =
+  | {
+      type: "DELETE_LIST";
+      modalProps: DeleteListModalProps;
+    }
+  | { type: "UPDATE_CARD"; modalProps: CardModalProps }
+  | { type: "RENAME_LIST"; modalProps: RenameListModalProps };
+
+export type KanbanContext = {
+  kanbanState: KanbanBoardState;
+  modalState: ModalContextState;
+  handleCreateList: (title: string) => void;
+  handleDeleteList: (listIndex: number) => void;
+  handleRenameList: (listIndex: number, title: string) => void;
+  handleCreateCard: (listIndex: number, title: string) => void;
+  handleDeleteCard: (listIndex: number, cardIndex: number) => void;
+  handleUpdateCard: (
+    listIndex: number,
+    cardIndex: number,
+    updatedCard: KanbanCard,
+  ) => void;
+  handleDragEnd: (dropResult: DropResult) => void;
+  handleOpenModal: (props: hanbleOpenModalProps) => void;
+  handleCloseModal: () => void;
+};
